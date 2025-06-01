cf08e1078b9b55ef9efc8d5c6d02da499454d248
Anwar12052006 <anwarraza6299@gmail.com>
2025-06-01 15:41:22 +0530

    add into Namaste-React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Header from "./components/Header";
-import Body from "./components/Body";
+import Header from './components/Header';
+import Body from './components/Body';
 
+/* Components of Our Food-Order App
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search Bar
+ * - Restaurant-Container
+ *  - Restaurant-Card
+ *    - Img
+ *    - Name of Res, Star Rating, cuisine, delivery time.
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ */
 
 //  const styleCard = {
 //   backgroundColor: '#f0f0f0',
@@ -25,18 +41,25 @@ import Body from "./components/Body";
 // const RestaurantCard = ({ resName, cuisine }) => {
 //   console.log({ resName, cuisine });
 
-
-
-
 // * not using keys (not acceptable) <<<< index as a key <<<<<<<<<< unique id (is the best  practice)
-
-
 
 // * What is Config-driven-UI -> A "config-driven UI" is a user interface that is built and configured using a declarative configuration file or data structure, rather than being hardcoded.
 
 // * Every company now-a-days follows these approach, because our Appications need to be Dynamic These Days
 
 // * Note: A Good Senior Frontend engineer is - who is a good UI Layer Engineer and a good Data Layer Engineer
+
+/* Episode 05 Let's get Hookd ------------------------------------------------------------------------------------------------------ 
+ * There are 2 type of Export/Import
+ *   1) Defalut Export/Import
+ *        export default Component;
+ *        import Component from 'path';
+
+ *   2) Name Export/Import
+ *        export const Component;
+ *        import { Component } from 'path'; 
+ 
+*/
 
 const currYear = new Date().getFullYear();
 
@@ -51,6 +74,7 @@ const Footer = () => {
 };
 
 const AppLayout = () => {
+  // console.log(<Body />);
   return (
     <div className="app">
       <Header />
